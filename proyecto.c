0840a49db6d4e70caae9dbc98f7627e03e1d0d71
MigueBlogs <m_plqp2@hotmail.com>
2014-03-03 12:47:09 -0600

    Update and rename proyecto to proyecto.c

--- proyecto.c
+++ proyecto.c
@@ -8,19 +8,20 @@
 
 int main ()
 {
-    int x;
-
+    int x, y, z;
+    char b[20];
+    char d[20];
     char c;
     
     FILE *arch;
-    arch= fopen ("archivo.txt","a");
+    arch= fopen ("archivo.txt","r");
 do {        
     p("\n Selecciona una opcion del siguiente menu:");
-    p("\n 1.- Escribir texto y guardarlo");
+    p("\n 1.- Escribir una lista de elementos y guardarlo");
     p("\n 2.- Busqueda de una palabra en el texto");
     p("\n 3.- Borrar ciertos elementos del texto");
     p("\n 4.- Salir \n");
-    s("%d", x);
+    s("%d", &x);
     
    switch(x) 
    { 
@@ -30,14 +31,24 @@ do {
         p("404 ERROR - File not found!");
         }
     else{
-    
+   p("\nEscribe aqui tu lista de elementos que desees: \n Para cerrar el documento y guardarlo preciona '.' (Punto Final)\n");
    while ((c=getchar())!='.'){
    fputc(c, arch);  }
    fclose(arch);  
         }
     break;
     case 2: //// Busqueda
-         p(" \n\n\t  gracias por usar el programa \n\n\t ");
+         p(" \n  Ingrese el elemento a buscar y le dire si se encuentra en el documento: \n\n\t ");
+         fflush(stdin);
+         gets(b);
+         fgets(d, 10, arch);
+         z = strcmp(b,d);
+         if (z==0) {
+            p("\n Se encontro el elemento %c \n" ,b);}
+            else {
+            p("\n No se enconto el elemento  \n");
+                 }
+         system ("PAUSE");
          return 0;
     break;
     case 3: 
@@ -46,8 +57,8 @@ do {
          return 0;
     break;
     case 4: //// Salir
-         p(" \n\n\t  gracias por usar el programa \n\n\t ");
-         return 0;
+         p(" \n\n\t  Gracias por usar el programa \n\n\t ");
+         system ("PAUSE");
     break;
   default:                   
          p(" \n\n\t Elija una opcion correcta \n\n\t ");
@@ -61,4 +72,3 @@ do {
                              
                 system("PAUSE");                          
 }
-
