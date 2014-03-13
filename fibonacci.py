4f649e37271f860b2e86a488d9f730a793d82dd9
MigueBlogs <m_plqp2@hotmail.com>
2014-03-13 13:04:00 -0600

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,6 +1,11 @@
 ///////# Fibonaci & Factorial en código Phyton
+c=0
 f=[0,1,2,3,4,5,6,7,8,9]
-for i in range(9):
-    temp=f[i]
-    f[i]=temp+f[i+1]
-    print f[i],","
+def fibo(f):
+    for i in range (10):
+    if(f[i]<10):
+        print f[i]
+    else:
+        c=(fibo(f[i]-1)+fibo(f[i]-2))
+        print c
+        
