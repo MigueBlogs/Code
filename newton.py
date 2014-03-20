8ed8e033e59f35ecfc647bc27f9983a3bcdf5cd0
MigueBlogs <m_plqp2@hotmail.com>
2014-03-20 14:39:02 -0600

    newton.py 2

--- newton.py
+++ newton.py
@@ -2,5 +2,5 @@
 x = [1,2,4,5]
 y = [0,6,12,24]
 print " Determinaremos el polinomio interpolador por el metodo de las diferencias divididas de Newton para los puntos= "
-for i in range(x , y):
-    print "(x,y)"
+for i in range(4):
+    print "(" x "," y ")"
