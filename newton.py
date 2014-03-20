9e6e7b90819e00aae1b4f1c920b5bd9e78b7da91
MigueBlogs <m_plqp2@hotmail.com>
2014-03-20 14:57:47 -0600

    Update newton.py

--- newton.py
+++ newton.py
@@ -3,6 +3,4 @@ x = [1,2,4,5]
 y = [0,6,12,24]
 print " Determinaremos el polinomio interpolador por el metodo de las diferencias divididas de Newton para los puntos= "
 for i in range(4):
-    print x[i] 
-    print y[i] 
-    #print "(" x[i] "," y[i] ")"    <--- porque no puedo hacer esto? me marca ERROR..
+    print "(", x[i] ,",", y[i] ,")" 
