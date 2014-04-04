37a5de425cc228a7f9defea4840af101520f8c6a
MigueBlogs <m_plqp2@hotmail.com>
2014-04-04 14:56:01 -0600

    Create MediosTransporte .java

---
+++ MediosTransporte .java
@@ -0,0 +1,18 @@
+public class MediosTransporte
+{
+	double kilometros;
+	String dueno;
+	Integer num_pasajeros;
+	String modelo;
+
+
+public void frenar ()
+{
+	System.out.println(this.dueno+" freno en seco!");
+}
+public void acelerar ()
+{
+	System.out.println(this.modelo+" esta acelerando a maxima velocidad!");
+}
+
+}
